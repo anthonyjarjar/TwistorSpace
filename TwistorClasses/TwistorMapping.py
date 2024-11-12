42c3366211f9ea1782c1fd902839c49f232d8f68
anthonyjarjar <anthjar203@gmail.com>
2024-11-12 12:50:48 -0800

    Removed repetitive function

--- TwistorClasses/TwistorMapping.py
+++ TwistorClasses/TwistorMapping.py
@@ -1,6 +1,5 @@
 from TwistorClasses.ComplexNumber import ComplexNumber
 from TwistorClasses.ProjectivePoint import ProjectivePoint
-from math import acos
 
 
 def spacetime_to_twistor(t, x, y, z) -> ProjectivePoint:
@@ -18,26 +17,6 @@ def spacetime_to_twistor(t, x, y, z) -> ProjectivePoint:
     omega_1 = ComplexNumber(x, y)
     omega_2 = ComplexNumber(t + z, 0)
     pi_1 = ComplexNumber(1, 0) 
-    pi_2 = ComplexNumber(0, 1) 
-
-    return ProjectivePoint(omega_1, omega_2, pi_1, pi_2)
-
-def spacetime_null_direction_to_twistor(t, x, y, z) -> ProjectivePoint:
-    """Converts a null direction in spacetime to a twistor point.
-    
-    Args:
-        t (float): The time component of the spacetime point.
-        x (float): The x-coordinate in space.
-        y (float): The y-coordinate in space.
-        z (float): The z-coordinate in space.
-    
-    Returns:
-        ProjectivePoint: A twistor point in projective space representing the null direction.
-    """
-    
-    omega_1 = ComplexNumber(x, y)
-    omega_2 = ComplexNumber(t + z, 0)
-    pi_1 = ComplexNumber(1, 0)  
     pi_2 = ComplexNumber(0, 1) 
 
     return ProjectivePoint(omega_1, omega_2, pi_1, pi_2)
